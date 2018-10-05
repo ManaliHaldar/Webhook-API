b0360bc54cdc485655294bd40e056ce57e965d9c
ManaliHaldar <43747641+ManaliHaldar@users.noreply.github.com>
2018-10-05 22:06:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,8 +21,9 @@ restService.post("/echo", function(req, res) {
       ? req.body.result.parameters.echoText
       : "Seems like some problem. Speak again.";
   return res.json({
-    fulfillmentText: speech,
-    source: "EchoService"
+    speech: speech,
+    displayText: speech,
+    source: "webhook-echo-sample"
   });
 });
 
